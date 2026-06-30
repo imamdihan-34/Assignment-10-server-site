ae48a582c95bfe76a38094d46da798a98bf3520f
imamdihan-34 <imamdihan@gmail.com>
2026-06-30 21:21:50 +0600

    Implement JWT authentication and authorization middleware

--- src/config/stripe.js
+++ src/config/stripe.js
@@ -2,7 +2,7 @@ const stripeKey = process.env.STRIPE_SECRET_KEY;
 
 if (!stripeKey) {
   console.warn('⚠️ Warning: STRIPE_SECRET_KEY not found in .env file');
-  // Development এ dummy key দিয়ে চলতে দাও
+
   module.exports = require('stripe')('sk_test_dummy_for_dev');
 } else {
   module.exports = require('stripe')(stripeKey);
